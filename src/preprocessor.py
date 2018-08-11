8d576b005d834e3a1aac1fb862c88ad0a2b2324b
Chris Swinchatt <C.Swinchatt@Sussex.ac.uk>
2018-08-11 14:36:56 +0100

    Improve memory usage in preprocessor
    Previously, the entire input file was read into memory and records were assigned to N worker processes (where N=number of CPU threads), and the results were
    were written to file whenever they were ready. However, for larger input files, (>100 MB, which produces several GB of output), this used far too much memory.
    Now, only the number of records which can actually be processed in parallel are read, and no more records are read until a worker is available. As soon as any
    worker finishes encoding, its output is written to the disk and references to its input and output are deleted. This prevents holding more than N input and
    output records in memory at a time.

--- src/preprocessor.py
+++ src/preprocessor.py
@@ -17,7 +17,7 @@ from   mldisasm.io.training_set import DELIMITER, CHARS_PER_BYTE
 
 REPORT_STEP = 10000
 
-def pp_encode(record, tokens, x_codec, y_codec):
+def pp_encode(record, x_codec, y_codec):
     '''
     Encode a single record.
     :param record: The record.
@@ -40,6 +40,28 @@ def pp_encode(record, tokens, x_codec, y_codec):
     targets  = y_codec.encode(target, as_tensor=False)
     return json.dumps([inputs,targets])
 
+def write_available_results(results, tset_out, record_num):
+    '''
+    Wait until at least one result is available and then write it/them out to disk.
+    '''
+    # Collect at least one ready result.
+    avail = []
+    while not avail:
+        for i in range(len(results)):
+            if results[i].ready():
+                avail.append(i)
+    count = len(avail)
+    assert count > 0
+    # Delete the results in reverse order so we don't invalidate indices.
+    for i in reversed(avail):
+        line = results[i].get()
+        tset_out.write(line)
+        tset_out.write('\n')
+        del results[i]
+    if (record_num + count) % REPORT_STEP == 0:
+        log.info('Processed {} records'.format(record_num + count))
+    return count
+
 if __name__ == '__main__':
     if len(sys.argv) != 2:
         print(__doc__.format(sys.argv[0]))
@@ -54,32 +76,30 @@ if __name__ == '__main__':
     y_codec  = AsciiCodec(config['seq_len'], tokens)
     tset_in  = file_mgr.open_training_raw(model_name)
     tset_out = file_mgr.open_training_pp(model_name)
-    # Encode training set. Our strategy here is to read the records into memory one at a time and hand them to one of N
-    # workers (see pp_encode) where N is the number of CPU threads. mp.Pool handles allocating each line of input to a
-    # worker. Each worker produces a single line of JSON data for each record it's given and the results are stored
-    # asynchronously in a list. After queueing all the tasks (one per line of input) we write the results out in the
-    # order that they become available.
+    # Encode training set. Our strategy is to read one record per worker thread (n_threads), then wait for at least one
+    # of those workers to finish and write the results. This way we don't load the entire file into memory at once.
     record_num = 0
     n_threads  = mp.cpu_count()
     with mp.Pool(processes=n_threads) as pool:
         # Submit tasks and collect asynchronous results.
         log.info('Processing training file (spawning {} workers)'.format(n_threads))
-        results = []
+        results    = []
+        record_num = 0
+        active     = 0
         for record in tset_in:
             record = record[:-1] # Remove trailing newline.
-            results.append(pool.apply_async(pp_encode, (record, tokens, x_codec, y_codec)))
+            results.append(pool.apply_async(pp_encode, (record, x_codec, y_codec)))
+            active += 1
+            while active >= n_threads:
+                count = write_available_results(results, tset_out, record_num)
+                record_num += count
+                active     -= count
+                # Don't let active underflow.
+                if active < 0:
+                    log.warning('`active` counter underflowed')
+                    active = 0
         pool.close()
-        # Write the results of each task out to the file as they become available.
-        log.info('Waiting for results to become available')
+        # Wait for the rest of the results to become available.
+        log.info('Waiting for remaining {} results'.format(len(results)))
         while results:
-            for i in range(len(results)):
-                if results[i].ready():
-                    line = results[i].get()
-                    tset_out.write(line)
-                    tset_out.write('\n')
-                    if record_num and record_num % REPORT_STEP == 0:
-                        log.info('Processed {} records'.format(record_num))
-                    record_num += 1
-                    del results[i] # This invalidates the indices, so we need to loop again from the beginning.
-                    break
-        pool.join()
+            record_num += write_available_results(results, tset_out, record_num)
