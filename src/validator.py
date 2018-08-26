9c551e1d4c88e3a09c99469108519ffd13efe213
Chris Swinchatt <C.Swinchatt@Sussex.ac.uk>
2018-08-26 17:43:52 +0100

    Add accuracy metric to validator

--- src/validator.py
+++ src/validator.py
@@ -15,14 +15,13 @@ if __name__ == '__main__':
     # Filter out debug messages from TF.
     os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'
 
-import tensorflow               as tf
-import tensorflow.keras         as keras
-import tensorflow.keras.backend as K
+import tensorflow as tf
 
-from   mldisasm.io.codec        import AsciiCodec, BytesCodec
-from   mldisasm.io.file_manager import FileManager
-import mldisasm.util.log          as     log
-from   mldisasm.model           import Disassembler
+tf.logging.set_verbosity(tf.logging.INFO)
+
+from mldisasm.io.codec        import AsciiCodec
+from mldisasm.io.file_manager import FileManager
+from mldisasm.model           import Disassembler
 
 if __name__ == '__main__':
     # Read the command line.
@@ -30,35 +29,50 @@ if __name__ == '__main__':
         print(__doc__.format(sys.argv[0]), file=sys.stderr)
         exit(1)
     model_name = sys.argv[1]
-    tf.logging.set_verbosity(tf.logging.INFO)
     # Load files and create codecs.
     file_mgr   = FileManager()
     config     = file_mgr.load_config()
     tokens     = file_mgr.load_tokens()
     seq_len    = config['seq_len']
     mask_value = config['mask_value']
-    x_codec    = BytesCodec(seq_len, mask_value)
     y_codec    = AsciiCodec(seq_len, mask_value, tokens)
     # NB: Creating a new model and loading the weights into it works around a bug in keras.models.load_model(). This
     # will fail if the model configuration (number of units or layers) changes between saving and loading the model.
     model = Disassembler(**config['model'])
     # pylint: disable=protected-access
     model.load_weights(file_mgr._qualify_model(model_name))
-    # Perform validation.
-    sample  = 1
-    losses = []
-    session = K.get_session()
-    with session.as_default():
-        for X, y_true in file_mgr.yield_validation(model_name, y_codec):
-            # Convert to NumPy arrays.
-            X       = np.asarray([X])
-            y_true  = np.asarray([y_true])
-            # Compute predictions and loss.
-            y_pred  = model.predict(X)
-            loss    = model.test_on_batch(X, y_true)
-            # Decode and print results.
-            y_true  = ''.join(y_codec.decode(y_true)).rstrip()
-            y_pred  = ''.join(y_codec.decode(y_pred)).rstrip()
-            losses.append(loss)
-            print('Sample {}: loss={}, avg_loss={}, y_pred=\'{}\', y_true=\'{}\''.format(sample, loss, np.mean(losses), y_pred, y_true))
-            sample += 1
+    # Perform validation line-by-line.
+    sample = 1
+    losses = [None]*config['max_records']
+    accs   = [None]*config['max_records']
+    for X, y_true in file_mgr.yield_validation(model_name, y_codec):
+        X      = np.asarray([X])
+        y_true = np.asarray([y_true])
+        # Compute predictions and loss.
+        y_pred  = model.predict(X)
+        metrics = model.test_on_batch(X, y_true)
+        if model.metrics_names == ['acc','loss']:
+            acc, loss = metrics
+        elif model.metrics_names == ['loss','acc']:
+            loss, acc = metrics
+        else:
+            raise ValueError('Unrecognised metrics names: {}'.format(','.join(model.metrics_names)))
+        accs.append(acc)
+        losses.append(loss)
+        # Decode and print results.
+        y_true = ''.join(y_codec.decode(y_true)).rstrip()
+        y_pred = ''.join(y_codec.decode(y_pred)).rstrip()
+        print('Sample {}: loss={}, avg_loss={}, acc={}%, avg_acc={}%, y_pred="{}", y_true="{}"'.format(
+            sample,
+            loss,
+            np.mean(losses),
+            acc,
+            np.mean(accs),
+            y_pred,
+            y_true
+        ))
+        sample += 1
+    print('Validated {} samples')
+    print('          MIN\tMEAN\tMAX')
+    print('Accuracy: {}%\t{}%\t{}%'.format(min(accs), np.mean(accs),   max(accs)))
+    print('Loss:     {}\t{}\t{}'.format(min(losses),  np.mean(losses), max(losses)))
